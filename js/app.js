b87546bb06c511578b881822362bf35b9b1a6a61
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-07-20 15:38:42 -0600

    jusst missing the last step

--- js/app.js
+++ js/app.js
@@ -13,3 +13,26 @@ function removeTag(details) {
 
 let h1_Tag_exit = document.getElementById(`h1_tag`);
 h1_Tag_exit.addEventListener(`mouseleave`, removeTag);
+
+function toPurple(details) {
+    if(details[`key`] === `p`) {
+        document.body[`style`][`backgroundColor`] = `purple`;
+    }
+}
+
+window.addEventListener(`keydown`, toPurple);
+
+function toWhite(details) {
+    if(details[`key`] === `p`) {
+        document.body[`style`][`backgroundColor`] = `white`;
+    }
+}
+
+window.addEventListener(`keyup`, toWhite);
+
+function changeImg(details) {
+    img.setAttribute(`src`, `https://images.unsplash.com/photo-1486870591958-9b9d0d1dda99?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80`);
+}
+
+let img = document.querySelector(`img`);
+img.addEventListener(`click`, changeImg);
